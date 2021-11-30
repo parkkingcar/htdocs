e2c59edd1b25150e84436b4a36b9214b2a91a494
ww5862 <89507788+ww5862@users.noreply.github.com>
2021-11-30 19:54:28 +0900

    menu.css Menu.html

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
+<html lang="ko">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/homepage.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
-<body style="background-image: url('img/001.png');">
+<body style="background-image: url('img/001.png');background-repeat: no-repeat;">
 <?php include 'Menu.html';?>
+<!--소개말 -->
+<div class="intro">
+    <div id="intor-sub">
+        <h2 id="first-intro"><span id="first">No.01</span> 00산업</h2>
+        <h1 id="second-intro">00산업 최고의 제품을 만나보세요.</h1>
+        <p id="third-intro">완벽한 기술력과 서비스로 한차원 높은 품질을 지켜가겠습니다.</p>
+    </div>
+</div>
+
+<div id="second">
+
+</div>
 
 </body>
 
